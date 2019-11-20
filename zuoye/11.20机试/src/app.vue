48a929d672e9e79f5dfed02648c5af298bf2f4c6
sumeixing <2215131072@qq.com>
2019-11-20 20:38:27 +0800

    第二次周考

---
+++ zuoye/11.20机试/src/app.vue
@@ -0,0 +1,278 @@
+<template>
+	<div id="app">
+		<div class="one-box">
+			<div class="head">
+				<p>起飞页自助建站平台</p>
+				<div class="title">
+					<span style="color:#FFFFFF;">与创业为伴,点亮创新,</span>
+					<span style="color:red;">是云建站的使命</span>
+				</div>
+				<p class="zi">自助建站平台</p>
+				<!--<div>
+					<img v-for="i in arr">
+				</div>-->
+			</div>
+			
+		</div>
+		
+	<div class="column_containter" style="z-index:3;position:relative;margin-top:40px;">
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-50493 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_top-to-bottom qfe_start_animation" style="position: relative;;;line-height:3em;;background-repeat: no-repeat;;">
+  <div class="qfe_wrapper">
+   <div style="position: relative;">
+<div><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255); text-align: center; display: block;margin-top:50px;"></span><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; text-align: center; display: block;"><span style="line-height:1;"><span style="font-family: 微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(232, 76, 61);"><span style="word-wrap: break-word; word-break: break-all; font-size: 30px;">您的创业梦想需要一个非同凡响的网站</span></span></span></span></span></div>
+</div>
+
+  </div> 
+ </div> 
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-62168 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_bottom-to-top qfe_start_animation" style="position: relative;;;line-height:1.5em;;background-repeat: no-repeat;;margin-top:25px;">
+  <div class="qfe_wrapper">
+   <div>
+<div style="text-align: center;"><span style="font-family:微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(105, 105, 105);">云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</span></span></div>
+</div>
+
+  </div> 
+ </div> </div>
+ <div style="width: 90%; height: 250px; margin: 50px auto;overflow: hidden;">
+    <div style="width: 30%;height: auto;text-align: center;float: left;">
+     <img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvZjgxMjkwZTg3YTEzM2ZkNWFiM2IzY2ExNjdhNTA3ZDgucG5n.png"
+      alt="">
+     <p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">页面要炫酷,颜值要高</p >
+     <p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">人靠衣装马靠鞍,一个炫酷的网站才配得上创业者的新潮创
+      意.微企点采用HTML5最新技术,拥有大气舒适的界面和众多 精美模板,用起飞页制作一个高逼格.高颜值的炫酷网站, 让您的创业项目脱颖而出,非同凡响.</p >
+
+    </div>
+    <div style="width: 30%;height: auto;text-align: center;margin: 0 auto;">
+     <img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvY2JmYjViODAyM2M4ZjczMzU4YmY0OGIxMmEwNmRjNDkucG5n.png"
+      alt="">
+     <p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">简单易用,随需而变</p >
+     <p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">互联网创业瞬息万变,天下武功,唯快不破.创业者可以按需选择起飞页中精美的网页模板,预置栏目,快速添加和编辑网页无需编程,一步即创建一个炫酷高颜值的网站.网站从无到有,倚马可待.</p >
+
+    </div>
+    <div style="width: 30%;height: auto;text-align: center;float: right;margin-top: -205px;">
+     <img src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvMzY3N2Y5N2FhMjQ5YThmNmFlODUxNGJiM2E1NWExZGEucG5n.png"
+      alt="">
+     <p style="font-size: 14px;color: #B2B2B2;padding: 10px 0px;">支持多终端跨屏展示</p >
+     <p style="font-size: 12px;color: #B2B2B2;line-height: 20px;">在移动互联网全面渗透时,创业者需要一个随屏幕大小不同而自动伸缩的网站,这样同一份内容都可为不同终端用户带来最佳用户体验.微企点使用最新的HTML5响应式技术,网页界面随不同屏幕尺寸自动伸缩.</p >
+
+    </div>
+   </div>
+   <!---->
+		<div style="width:100%;height:300px;background:rgba(6,6,6,0.6);">
+			<div style="background:rgba(5,5,5,0.3);min-height: 100px;">
+			<h3 style="text-align: center;line-height:56px;color:#FFFFFF;font-size:24px;">云建站自助平台</h3>
+			<p style="color:#e84c3d;text-align: center;font-size:24px;">上手快.颜值高.跨多屏.自传播.全免费.</p>
+			</div>
+			<div style="width:50%;margin:0 auto;margin-top:50px;">
+				<p style="color:#FFFFFF">在起飞页自助建站平台，您不需要任何编程基础，就能做出功能强大，效果出众的专业级网站，您所要做的仅是选择网站模板、输入图片文字，以及鼠标拖拉拽；真正的响应式建站，无论是在电脑的大屏幕，还是手机小屏幕上，网站都能友好地展示；您的访客还可以注册成为会员，并进行在线购物；当然，如果您需要多语言版本，起飞页也可以轻松为您实现。</p>
+			</div>
+		</div>
+	<!--云建站-->
+		<div class="column_containter" style="z-index:3;position:relative;">
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-50493 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_top-to-bottom qfe_start_animation" style="position: relative;;;line-height:3em;;background-repeat: no-repeat;;">
+  <div class="qfe_wrapper">
+   <div style="position: relative;">
+<div><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255); text-align: center; display: block;margin-top:20px;"></span><span style="color: rgb(51, 51, 51); font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; text-align: center; display: block;"><span style="line-height:1;"><span style="font-family: 微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(232, 76, 61);"><span style="word-wrap: break-word; word-break: break-all; font-size: 30px;">云建站，一站式的在线H5建站平台</span></span></span></span></span></div>
+</div>
+
+  </div> 
+ </div> 
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-62168 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_bottom-to-top qfe_start_animation" style="position: relative;;;line-height:1.5em;;background-repeat: no-repeat;;margin-top:25px;">
+  <div class="qfe_wrapper">
+   <div>
+<div style="text-align: center;"><span style="font-family:微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(105, 105, 105);">云建站倾力打造的一个从网站建设.网网站运营管理到网站推广的自动化免费建站平台</span></span></div>
+</div>
+
+  </div> 
+  <div style="margin:0 auto;width:800px;height:645px;margin-top:20px;">
+  	<img style="width:100%;height:645px;" src="https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDIvNTE1Y2RhYTQxNDg0YWRiZDY4Mjg1NmU0OGNkZjFlYWYucG5n.png">
+  </div>
+ </div> 
+</div>
+		
+		
+<div class="k">
+	<div style="width:80%;margin:0 auto;">
+		<div class="h">	
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+		<div class="h">
+			<img src="https://i.52112.com/icon/jpg/256/20191107/66036/2864676.jpg">
+			<h5>跨屏展示</h5>
+			<p>一个炫酷的网站才配得上创业者的新潮创意.起飞页采用HTML5最新技术,拥有大气舒适的界面和众多精美模板</p>
+		</div>
+	</div>
+</div>			
+		
+		
+<div class="column_containter" style="z-index:3;position:relative;">
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-50493 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_top-to-bottom qfe_start_animation" style="position: relative;;;line-height:3em;;background-repeat: no-repeat;;">
+  <div class="qfe_wrapper">
+   <div style="position: relative;">
+<div><span style="color: rgb(51, 51, 51);font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; background-color: rgb(255, 255, 255); text-align: center; display: block;">
+	
+</span><span style="color: rgb(51, 51, 51); margin-top:20px; font-family: 微软雅黑, Verdana, Arial, Helvetica, sans-serif; font-size: 14px; text-align: center; display: block;"><span style="line-height:1;"><span style="font-family: 微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(232, 76, 61);"><span style="word-wrap: break-word; word-break: break-all; font-size: 30px;">为创业者而生的免费建站平台</span></span></span></span></span></div>
+</div>
+
+  </div> 
+ </div> 
+ <div m-padding="0px 0px 0px 0px" p-padding="0px 0px 0px 0px" css_animation_delay="0" qfyuuid="0" class="qfy-element qfy-text qfy-text-62168 qfe_text_column qfe_content_element  qfe_animate_when_almost_visible qfe_bottom-to-top qfe_start_animation" style="position: relative;;;line-height:1.5em;;background-repeat: no-repeat;;margin-top:25px;">
+  <div class="qfe_wrapper">
+   <div>
+<div style="text-align: center;"><span style="font-family:微软雅黑;"><span style="word-wrap: break-word; word-break: break-all; color: rgb(105, 105, 105);">用云建站平台,上手快.颜值高.跨多屏.自传播.全免费.</span></span></div>
+</div>
+
+  </div> 
+ </div> </div>		
+		
+	
+		
+		
+<template>
+ <div>
+  <!-- <div id="container"></div> -->
+  <div id="allmap" style="margin: auto;"></div>
+ </div>
+ 
+ 
+ 
+ 
+ 
+ 
+</template>
+	
+	</div>
+
+</template>
+
+
+<script>
+ export default {
+  mounted() {
+   var map = new BMap.Map("allmap");
+   // var point = new BMap.Point(116.331398, 39.897445);北京的经纬度
+   map.addControl(new BMap.NavigationControl());
+   map.addControl(new BMap.ScaleControl());
+   map.addControl(new BMap.OverviewMapControl());
+   map.addControl(new BMap.MapTypeControl());
+   map.centerAndZoom('苏州市外河桥', 11);
+  }
+ }
+</script>
+
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+	
+	.one-box {
+		width: 100%;
+		min-height:700px;
+		background-image: url('https://ccdn.goodq.top/caches/38ad257d4bd31c1e897c7f2f4f0c83f3/aHR0cHM6Ly81NmM0MmI1MDY3NmYxLnQ3My5xaWZlaXllLmNvbS9xZnktY29udGVudC91cGxvYWRzLzIwMTYvMDMvNzIzNGQ1MjBkZjBiODhiMDIwYTc3ZmYwZjk1MGM3OGEuanBn.jpg');
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+	}
+	
+	.head {
+		position: relative;
+	}
+	
+	.head p {
+		font-size: 60px;
+		font-weight: normal;
+		font-style: normal;
+		color: #ffffff;
+		position: absolute;
+		top: 210px;
+		left:32%;
+	}
+	
+	.head .title {
+		height: 100px;
+		/*background: red;*/
+		font-size: 36px;
+		text-align: center;
+		line-height: 100px;
+		position: absolute;
+		top: 350px;
+		left:30%;
+		border-top:2px solid #FFFFFF ;
+		border-bottom:2px solid #FFFFFF ;
+		
+	}
+	.head .zi{
+		position: absolute;
+		top:500px;
+		left:40%;
+		font-size:20px;
+	}
+#allmap {
+  width: 100%;
+  height: 500px;
+  float: left;
+  /*padding: 0 20px;*/
+  background: #004444;
+ }
+.k{
+ 	
+ 	width:100%;
+ 	height:500px;
+ 	background:rgba(1,1,1,0.1);
+}
+.k .h{
+	width:20%;
+	float:left;
+	/*background:red;*/
+	margin:1%;
+	margin-top:6%;
+	margin-left:4%;
+}
+.h img{
+	width:50px;
+	height:50px;
+	margin-left:40%;
+}
+.h h5{
+	font-size: 16px;
+	text-align: center;
+	color:#c2c2c2;
+	
+}
+.h p{
+	font-size:12px;
+	color:#a9a9a9;
+}
+</style>
